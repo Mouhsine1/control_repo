9891ce5d9f1a5bc6e49feb769bffbeb2cc1c3ffc
Mouhsine1 <42320991+Mouhsine1@users.noreply.github.com>
2019-11-25 16:40:03 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,6 @@
 node default{
-file{'/root/readme':
-  ensure => file,
-  content => 'this is my first play with puppet and i am going to try to go far then you think an you will see the ruslts and  i will win',
-  owner => 'root'
-}
-user{'yasmine':
-  ensure => present,
 
-}
+include role:: master_server  
 
 
 }
