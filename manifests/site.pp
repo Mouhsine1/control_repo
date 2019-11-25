2fb5494547477ed009cf035ca7a1982bd1cb4508
Mouhsine1 <42320991+Mouhsine1@users.noreply.github.com>
2019-11-25 15:50:09 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,8 +2,11 @@ node default{
 file{'/root/readme':
   ensure => file,
   content => 'this is my first play with puppet and i am going to try to go far then you think an you will see the ruslts and  i will win',
-  
+}
 
+file {'/root/readme':
+
+ owner => 'root'
 
 }
 
