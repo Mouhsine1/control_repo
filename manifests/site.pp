01deec7aab91fde0d6347e74d1b654a353cdabee
Mouhsine1 <42320991+Mouhsine1@users.noreply.github.com>
2019-11-25 15:43:18 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node default{
 file{'/root/readme':
   ensure => file,
-  content => 'this is my first play with puppet and i am going to try to go far then you think an you will see the ruslts and  i will win'
+  content => 'this is my first play with puppet and i am going to try to go far then you think an you will see the ruslts and  i will win',
   
 
 
